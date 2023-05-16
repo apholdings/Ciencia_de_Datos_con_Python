f531e6ac845a4da50d050d28dfd70ba33a1f385d
SoloPython <alexander.eric.3@gmail.com>
2023-05-16 11:27:36 -0500

    numpy

---
+++ 2) Introduccion a la Ciencia de Datos/numpy.py
@@ -0,0 +1,10 @@
+# Importación de NumPy
+import numpy as np
+
+# Creación de una matriz en NumPy
+matriz = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(matriz)
+
+# Realizar operaciones en la matriz
+print("Suma de todos los elementos:", np.sum(matriz))
+print("Transpuesta de la matriz:\n", np.transpose(matriz))
